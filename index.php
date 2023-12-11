f69c2a8896558b19251aa253fe4374b07aa8499e
Iqbal Dhiya Ul Haqq <dhiyaulhaqq@gmail.com>
2023-12-11 16:30:41 +0700

    initial

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" href="assets/css/fontawesome-all.css">
+    <link rel="stylesheet" href="assets/css/theme.css">
+</head>
+
+<body>
+    <div id="map">
+        <div class="block-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d4130971.4810411297!2d-0.7579433850719401!3d51.507400000000004!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1702285183996!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="floating-content">
+            <div class="floating-box">
+                <div class="control-box box-shadow">
+                    <div class="search d-flex mb-3">
+                        <div class="border border-primary rounded-lg d-flex bg-white w-100 mr-2">
+                            <input type="text" class="form-control border-white" placeholder="Click the target or type a location">
+                            <button class="btn"><i class="fa-solid fa-location-crosshairs"></i></button>
+                        </div>
+                        <button class="btn bg-white border-primary w-25 rounded-pill"><i class="fa-solid fa-location-dot mr-2"></i>Saved (0)</button>
+                    </div>
+                    <div class="filter">
+                        <button class="btn bg-white border-primary rounded-pill">Open Now</button>
+                        <button class="btn bg-white border-primary rounded-pill">Filter Date</button>
+                        <button class="btn bg-white border-primary rounded-pill">Reset Map</button>
+
+                    </div>
+                </div>
+                <div class="landing-box box-shadow">
+                    <div class="box-content">
+                        <h3>Christmas Markets in Europe</h3>
+                        <p>An interactive European map showcasing the most enchanting Christmas markets.</p>
+
+                        <p>Plan your festive journey - virtually or in person.</p>
+
+                        <p>Pick your favourite markets and enter a city to explore the Yuletide wonders nearby - all from the comfort of your home! Check out the magical stalls of Nuremberg, wander the twinkling pathways of Strasbourg, or delve into the festive delights of Vienna's Christkindlmarkt.</p>
+
+                        <p class="note">Please note that this page should only be used as a guidance as Christmas market opening times may be subject to change during the festive season. We advise that you check the individual market's official website or contact the organisers directly for the most up-to-date timings.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
