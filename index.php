5803b3c5a5124b29a7c396700b01fc82a3cbf864
Iqbal Dhiya Ul Haqq <dhiyaulhaqq@gmail.com>
2023-12-13 09:30:31 +0700

    floating container & box-shadow

--- index.php
+++ index.php
@@ -13,34 +13,32 @@
     <div class="page-section section-map">
         <div id="map">
         </div>
-        <div class="floating-content">
-            <div class="floating-box">
-                <div class="control-box box-shadow">
-                    <div class="search d-flex flex-wrap justify-content-center">
-                        <div class="border border-primary rounded-lg d-flex flex-fill bg-white px-1 mr-2 mb-3">
-                            <input type="text" class="flex-fill border-white" placeholder="Click the target or type a location">
-                            <button class="btn"><i class="fa-solid fa-location-crosshairs"></i></button>
-                        </div>
-                        <button class="btn bg-white border-primary rounded-pill mb-3"><i class="fa-solid fa-location-dot mr-2"></i>Saved (0)</button>
+        <div class="floating-container">
+            <div class="control-box box-shadow">
+                <div class="search d-flex flex-wrap justify-content-center">
+                    <div class="border border-primary rounded-lg d-flex flex-fill bg-white px-1 mr-2 mb-3">
+                        <input type="text" class="flex-fill border-white" placeholder="Click the target or type a location">
+                        <button class="btn"><i class="fa-solid fa-location-crosshairs"></i></button>
                     </div>
-                    <div class="filter block-hor">
-                        <button class="btn bg-white border-primary rounded-pill item">Open Now</button>
-                        <button class="btn bg-white border-primary rounded-pill item">Filter Date</button>
-                        <button class="btn bg-white border-primary rounded-pill item">Reset Map</button>
-
-                    </div>
+                    <button class="btn bg-white border-primary rounded-pill mb-3"><i class="fa-solid fa-location-dot mr-2"></i>Saved (0)</button>
                 </div>
-                <div class="landing-box box-shadow">
-                    <div class="box-content">
-                        <h3>Christmas Markets in Europe</h3>
-                        <p>An interactive European map showcasing the most enchanting Christmas markets.</p>
+                <div class="filter block-hor">
+                    <button class="btn bg-white border-primary rounded-pill item">Open Now</button>
+                    <button class="btn bg-white border-primary rounded-pill item">Filter Date</button>
+                    <button class="btn bg-white border-primary rounded-pill item">Reset Map</button>
 
-                        <p>Plan your festive journey - virtually or in person.</p>
+                </div>
+            </div>
+            <div class="landing-box box-shadow">
+                <div class="box-content">
+                    <h3>Christmas Markets in Europe</h3>
+                    <p>An interactive European map showcasing the most enchanting Christmas markets.</p>
 
-                        <p>Pick your favourite markets and enter a city to explore the Yuletide wonders nearby - all from the comfort of your home! Check out the magical stalls of Nuremberg, wander the twinkling pathways of Strasbourg, or delve into the festive delights of Vienna's Christkindlmarkt.</p>
+                    <p>Plan your festive journey - virtually or in person.</p>
 
-                        <p class="note">Please note that this page should only be used as a guidance as Christmas market opening times may be subject to change during the festive season. We advise that you check the individual market's official website or contact the organisers directly for the most up-to-date timings.</p>
-                    </div>
+                    <p>Pick your favourite markets and enter a city to explore the Yuletide wonders nearby - all from the comfort of your home! Check out the magical stalls of Nuremberg, wander the twinkling pathways of Strasbourg, or delve into the festive delights of Vienna's Christkindlmarkt.</p>
+
+                    <p class="note">Please note that this page should only be used as a guidance as Christmas market opening times may be subject to change during the festive season. We advise that you check the individual market's official website or contact the organisers directly for the most up-to-date timings.</p>
                 </div>
             </div>
         </div>
